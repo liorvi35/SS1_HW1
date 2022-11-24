2997c71bd99f6d81bbb68268e4c1f2cdecd2722b
liorvi35 <liorvi35@gmail.com>
2022-11-24 16:05:36 +0200

    Update main.c

--- main.c
+++ main.c
@@ -17,27 +17,32 @@ int main()
 	int low = 0, high = 0;
 	scanf("%d %d", &low, &high);
 	printf("The Armstrong numbers are:");
-	for(int i = low; i <= high; i++){
+	for(int i = low; i <= high; i++)
+	{
 		if(isArmstrong(i) == 1)
 			printf(" %d", i);
 	}
 	printf("\n");
-	printf("The Palindromes numbers are:");
-	for(int i = low; i <= high; i++){
+	printf("The Palindromes are:");
+	for(int i = low; i <= high; i++)
+	{
 		if(isPalindrome(i) == 1)
 			printf(" %d", i);
 	}
 	printf("\n");
 	printf("The Prime numbers are:");
-	for(int i = low; i <= high; i++){
+	for(int i = low; i <= high; i++)
+	{
 		if(isPrime(i) == 1)
 			printf(" %d", i);
 	}
 	printf("\n");
 	printf("The Strong numbers are:");
-	for(int i = low; i<=high; i++){
+	for(int i = low; i <= high; i++)
+	{
 		if(isStrong(i) == 1)
 			printf(" %d", i);
 	}
+	print("\n");
 	return 0;
 }
